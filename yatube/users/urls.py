10d2739d57db3b8f91856b6c275004a7de288ae3
sparzha44 <henryrus.ilya@gmail.com>
2026-04-07 13:31:22 +0300

    Add files via upload

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -5,17 +5,12 @@ from . import views  # добавили импорт модуля views
 app_name = 'users'
 
 urlpatterns = [
-    path(
-      'logout/',
-      # Прямо в описании обработчика укажем шаблон,
-      # который должен применяться для отображения возвращаемой страницы.
-      # Да, во view-классах так можно! Как их не полюбить.
-      LogoutView.as_view(template_name='users/logged_out.html'),
-      name='logout'
-    ),
-        # Полный адрес страницы регистрации - auth/signup/,
-    # но префикс auth/ обрабатывется в головном urls.py
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path(
+        'logout/',
+        LogoutView.as_view(template_name='users/logged_out.html'),
+        name='logout'
+    ),
     path(
         'login/',
         LoginView.as_view(template_name='users/login.html'),
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates', 'css'),
+]
 
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
